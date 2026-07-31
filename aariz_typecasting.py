5de434092285ccfa3998769da6a9b88a3dcd1733
aarizrasul9-lab <aariz.rasul9@gmail.com>
2026-07-30 21:05:52 -0400

    Add files via upload

---
+++ aariz_typecasting.py
@@ -0,0 +1,17 @@
+name = "Aariz"
+age = 15
+is_student = True
+weight = 108.43
+print("Name :", name)
+print("Data Type of Name is", type(name))
+print("Age :", age)
+print("Data Type of is_student is", type(is_student))
+print("Date Type of Age is", type(age))
+print("Weight is", weight)
+print("Data Type", type(weight))
+age = str(age)
+print(age)
+print("Data Type of Age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
